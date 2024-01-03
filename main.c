5f48289ae43c0c98315d5f6397f3de0e763b66d1
Yogatheaitech <yogarajanai01@gmail.com>
2024-01-03 12:31:51 +0530

    modified main .c in input of logical operators

--- main.c
+++ main.c
@@ -98,7 +98,7 @@ int ComparisonOperatorsProgram()
 int logicalOperatorsProgram()
 {
     printf("LOGICAL OPERATORS PROGRAM:");
-     int a=11;
+     int a=21;
     if(a>10 && 10<a)
 {
     printf("\n\tThe logical AND Operation is true.\n");
